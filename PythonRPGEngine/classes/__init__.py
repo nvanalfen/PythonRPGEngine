c2e66dd2071f2ddc2dbaa2e8280302a5369016ed
Nick Van Alfen <nvanalfen2@gmail.com>
2023-03-18 23:27:48 +0100

    Reorganized init and added new files

--- PythonRPGEngine/classes/__init__.py
+++ PythonRPGEngine/classes/__init__.py
@@ -1,8 +1,10 @@
-from .enum_classes import Element, AttackType, CombatStyle, Race, WeaponType, EquipmentType, TargetType
-from .stats import Stats
-from .requirement import Requirement
-from .buff import Buff
-from .skill import Skill
-from .item import Item, Consumable, Equipment, Weapon
 from .attack import Attack
+from .buff import Buff
+from .character import Character
+from .convenience_classes import Backpack, Outfit
 from .damage import Damage
+from .enum_classes import Element, AttackType, CombatStyle, Race, WeaponType, EquipmentType, TargetType
+from .item import Item, Consumable, Equipment, Weapon
+from .requirement import Requirement
+from .skill import Skill
+from .stats import Stats
